393a3b37949833b9d4da3e996c53379791ad1237
tr1x_em <admin@trix.is-a.dev>
2026-02-26 12:25:24 +0530

    refactor(cli): Improve error handling

--- alienfx-cli/include/const.h
+++ alienfx-cli/include/const.h
@@ -4,26 +4,9 @@
 
 #include "AlienFX_SDK.h"
 
-struct ZONECODE {
-    std::string_view name;
-    unsigned code;
-};
-
 struct ACTIONCODE {
     std::string_view name;
     int afx_code;
-};
-
-#define ZONE_LEFT 0x00
-#define ZONE_MIDDLE_LEFT 0x01
-#define ZONE_MIDDLE_RIGHT 0x02
-#define ZONE_RIGHT 0x03
-
-inline constexpr ZONECODE zonecodes[]{
-    {"left", ZONE_LEFT},
-    {"middle_left", ZONE_MIDDLE_LEFT},
-    {"right", ZONE_RIGHT},
-    {"middle_right", ZONE_MIDDLE_RIGHT},
 };
 
 inline constexpr ACTIONCODE actioncodes[]{
